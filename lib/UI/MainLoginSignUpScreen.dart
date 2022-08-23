2fa3578e74ca8db6c51ae44bf2c5db563a664731
krishbala905 <vanikrishnant12@gmail.com>
2022-08-23 17:33:02 +0530

    Ui folder and main login singup file Added

---
+++ lib/UI/MainLoginSignUpScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class MainLoginUi extends StatefulWidget {
+  const MainLoginUi({Key? key}) : super(key: key);
+
+  @override
+  State<MainLoginUi> createState() => _MainLoginUiState();
+}
+
+class _MainLoginUiState extends State<MainLoginUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
