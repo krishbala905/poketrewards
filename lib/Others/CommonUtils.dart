a165e1e7afbee62000c10bebc9b720d1a0c34f13
krishbala905 <vanikrishnant12@gmail.com>
2022-08-23 15:54:31 +0530

    Common utils and utils file Added

---
+++ lib/Others/CommonUtils.dart
@@ -0,0 +1,98 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:toast/toast.dart';
+
+
+
+
+void showToast(String msg, {int? duration, int? gravity}) {
+  Toast.show(msg, duration: duration, gravity: gravity);
+}
+void hideKeyboard(){
+  SystemChannels.textInput.invokeMethod('TextInput.hide');
+}
+
+hexStringToColor(String hexColor) {
+  hexColor = hexColor.toUpperCase().replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+  return Color(int.parse(hexColor, radix: 16));
+}
+
+getRandomNumberBetweenAndToCharacter() {
+  int min = 71;
+  int max = 90;
+  Random foo = new Random();
+  int randomNumber = foo.nextInt(max - min) + min;
+  if (randomNumber == min) {
+    return (min + 1);
+  } else {
+    return randomNumber;
+  }
+}
+
+class CommonUtils{
+
+  static  String KEY_FORCE_UPDATE = "force_update";
+  static  String KEY_FORCE_LOG_OUT = "forcelogout_format";
+  static  String KEY_WALLET_SYNC = "walletsync";
+  static  String KEY_WALLET_SYNC_NEW = "walletsyncnew";
+  static  String KEY_CALL_INBOX = "callinbox";
+  static  String KEY_CARD = "rm";
+  static  String KEY_VOUCHER = "rv";
+  static  String KEY_MEMBER_POINT_TRANSACTION = "mpt";
+  static  String KEY_FEEDBACK_POINT_TRANSACTION = "event_fb_pt";
+  static  String KEY_MEMBERSHIP_POPUP = "show_membership_popup";
+
+
+  static  String none = "none";
+  static  String rewards_popup = "rewardsPopup";
+  static  String memberShip_popup = "membershipPopup";
+  static  String inboxPage = "inboxPage";
+  static  String walletPage = "walletPage";
+
+
+
+
+
+  static String triggerUrl="triggerurl";
+  static String triggerPhone="triggerphone";
+  static String triggerCMS="cms";
+  static String triggerPDF="triggerPDF";
+  static String triggerNormal="normal";
+  static String triggerEmail="triggeremail";
+  static String triggerOneSubbaner="onesubbanner";
+  static String MerchantId="318";
+  static  int QRVERSION = 1;
+  static  int APPLICATIONID = 1;
+  static  int SELECTEDLANGUAGEPACKAGEID = 1;
+
+  static String? consumerID="";
+  static String? merchantID="318";
+  static String? consumerName="";
+  static String? consumerGender="";
+  static String? consumerProfileImageUrl="";
+  static String? consumermobileNumber="";
+  static String? consumerIntialScreen="";
+  static String? consumerEmail="";
+  static String? deviceTokenID="3907";
+  static String? COUNTRY_INDEX="65";
+  static String? deviceToken="";
+  static String? deviceType="";
+  static String? deviceModel="";
+  static String? osVersion="";
+  static String? softwareVersion="1.0";
+  static String? timeStamp="";
+  static String? timeZone="";
+  static String? consumerApplicationType="1";
+  static String? consumerLanguageId="1";
+
+  static var PPN_RESPONSE_CONTENT;
+
+
+}
+
+
+
---
+++ lib/Others/Utils.dart
@@ -0,0 +1,107 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/services.dart';
+
+import 'CommonUtils.dart';
+class Utils {
+
+
+  Future<void> getDeviceINFO() async {
+    var deviceData = <String, dynamic>{};
+    final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+    try {
+      if (Platform.isAndroid) {
+        CommonUtils.deviceType="2";
+        deviceData =
+            _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+
+        CommonUtils.osVersion='${deviceData['version.sdkInt']}';
+        CommonUtils.deviceModel='${deviceData['brand']}${deviceData['device']}';
+
+      }
+      else if (Platform.isIOS) {
+        CommonUtils.deviceType="1";
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      //  deviceData['version.systemVersion'];
+        CommonUtils.osVersion= "15.5";
+        CommonUtils.deviceModel=deviceData['model'];
+      }
+      else {
+
+      }
+
+
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'systemFeatures': build.systemFeatures,
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
+
+
+
+  String getTimeStamp(){
+    DateTime _now = DateTime.now();
+    CommonUtils.timeStamp='${_now.hour}:${_now.minute}:${_now.second}.${_now.millisecond}';
+    return CommonUtils.timeStamp.toString();
+  }
+
+  String getTimeZone(){
+    DateTime now = DateTime.now();
+    var timezone = now.timeZoneName;
+    var offset = now.timeZoneOffset;
+    CommonUtils.timeZone='timeZone: ${timezone}:${offset}';
+    return CommonUtils.timeZone.toString();
+  }
+
+}
