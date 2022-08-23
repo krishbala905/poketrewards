e9238ef3910af10005ac2a0d626303977fe167dc
Bhar-athi <111727113+Bhar-athi@users.noreply.github.com>
2022-08-23 17:17:49 +0530

    Splash Screen

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'Others/CommonUtils.dart';
+import 'Others/Utils.dart';
+import 'res/Colors.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -8,7 +13,40 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Utils().getDeviceINFO();
+  }
+  @override
+
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 400,
+                width: 600,
+                //height: MediaQuery.of(context).size.height,
+                // width: MediaQuery.of(context).size.width,
+                child: Image.asset('assets/splash.png',),
+              ),
+              SpinKitCircle(
+                color: corporateColor,
+                size: 30.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
