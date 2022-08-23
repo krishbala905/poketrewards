3c463c6ba849f0f11888990076f50f2d93ab641c
krishbala905 <vanikrishnant12@gmail.com>
2022-08-23 15:43:25 +0530

    Colors file Added

---
+++ lib/Colors.dart
@@ -0,0 +1,23 @@
+
+
+
+import 'package:flutter/material.dart';
+
+
+// const  corporateColor  = Color(0xFFff9800);
+const  corporateColor  = Color(0xFF000000);
+const  background      = Color(0xFFFFFFFF);
+const  textcolor       = Color(0xFFFFFFFF);
+const  hinttextcolor   = Color(0xFFFFFFFF);
+const  cursorcolor     = Color(0xFFFFFFFF);
+const  black           = Color(0xFF000000);
+const  grey           = Color(0xFF353535);
+const  lightgrey           = Color(0xFFAEAEAE);
+const  lightgrey2           = Color(0xFFEAEAEA);
+const  lightgrey3           = Color(0xFFF3F3F3);
+const  poketPurple           = Color(0xFF6A1A41);
+
+const  PoketMaincolo   = Color(0xFF00ADA7);
+const PoketNormalGreen = Color(0xFF39b2a9);
+const PoketBrightGreen = Color(0xFFA1E0A9);
+const PoketBrightGreen2 = Color(0xFF4caf50);
