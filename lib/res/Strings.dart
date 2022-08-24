62e58a607fd387b417774ecb2d35737089047cd7
Bhar-athi <111727113+Bhar-athi@users.noreply.github.com>
2022-08-24 10:34:16 +0530

    Splash Screen

---
+++ lib/res/Strings.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const login ="Login";
+const email = "Email";
+const password = "Password";
+const loginwith = "LOGIN WITH EMAIL";
+const forgot_password = "Forgot Password?";
