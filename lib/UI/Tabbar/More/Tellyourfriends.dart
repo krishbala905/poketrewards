13c032db41ea5d5890311fd4c3cec0060e7c22d2
Bhar-athi <111727113+Bhar-athi@users.noreply.github.com>
2022-08-25 11:27:55 +0530

    More Fragment

---
+++ lib/UI/Tabbar/More/Tellyourfriends.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Tellyourfriends extends StatefulWidget {
+  const Tellyourfriends({Key? key}) : super(key: key);
+
+  @override
+  State<Tellyourfriends> createState() => _TellyourfriendsState();
+}
+
+class _TellyourfriendsState extends State<Tellyourfriends> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
